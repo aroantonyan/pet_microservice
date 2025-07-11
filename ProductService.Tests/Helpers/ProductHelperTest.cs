2fda227e7fd25b1cc36b4d8737da17278d9e8eab
aroantonyan <arayikantonyan02@gmail.com>
2025-07-11 17:17:42 +0400

    added product test service

---
+++ ProductService.Tests/Helpers/ProductHelperTest.cs
@@ -0,0 +1,15 @@
+namespace ProductService.Tests.Helpers;
+
+public class ProductHelperTest
+{
+    [Theory]
+    [InlineData("Iphone", 1500, 1550)]
+    [InlineData("IPhone", 1500, 1560)]
+    [InlineData("IMac", 1500, 1560)]
+
+    public void AdjustPriceIfDesirable_ReturnsPricePlusFifty(string productName, double productPrice, double expectedPrice)
+    {
+        var result = ProductService.Helpers.ProductHelper.AdjustPriceIfDesirable(productName, productPrice);
+        Assert.Equal(expectedPrice, result);
+    }
+}
